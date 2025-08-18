c26ce3316114021734b9b4c28511b4e941340e07
Sachin Shet <sachinshet135@gmail.com>
2025-08-18 18:23:20 -0400

    created new macro

--- custom_postgres/macros/film_ratings_macro.sql
+++ custom_postgres/macros/film_ratings_macro.sql
@@ -7,12 +7,7 @@ WITH film_with_ratings AS (
         price,
         rating,
         user_rating,
-        CASE 
-            WHEN user_rating >= 4.5 THEN 'Excellent'
-            WHEN user_rating >= 4.0 THEN 'Good'
-            WHEN user_rating >= 3.0 THEN 'Average'
-            ELSE 'Poor'
-        END as rating_category
+        {{ generate_ratings() }}
     FROM {{ ref('films') }}
 ),
 
